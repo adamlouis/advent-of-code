6092b7c38b9344e4b18eafca0c52853e72aafcb6
Adam Suczewski <adam@suczewski.com>
2021-12-07 10:56:00 -0500

    fix

--- 2021/07/src/main.rs
+++ 2021/07/src/main.rs
@@ -2,41 +2,40 @@ fn main() {
     let input = std::fs::read_to_string("input.txt").unwrap();
     let lines: Vec<&str> = input.split("\n").collect();
 
-    let mut values: Vec<u32> = lines[0]
+    let mut values: Vec<i32> = lines[0]
         .split(",")
-        .map(|s| s.parse::<u32>().unwrap())
+        .map(|s| s.parse::<i32>().unwrap())
         .collect();
 
     println!["part 1 = {:?}", part1(&mut values)];
     println!["part 2 = {:?}", part2(&mut values)];
 }
 
-fn part1(values: &mut Vec<u32>) -> u32 {
+fn part1(values: &mut Vec<i32>) -> i32 {
     values.sort();
-    let median = values[values.len() / 2];
+    let median: i32 = if values.len() % 2 == 1 {
+        values[values.len() / 2]
+    } else {
+        if values[values.len() / 2] != values[values.len() / 2 - 1] {
+            panic!("ah!");
+        }
+        values[values.len() / 2]
+    };
 
-    if values[values.len() / 2] != values[values.len() / 2 - 1] {
-        panic!("ah!");
-    }
-
-    let mut fuel: u32 = 0;
+    let mut fuel: i32 = 0;
     for v in (&values).iter() {
-        let f = if *v < median {
-            median - *v
-        } else {
-            *v - median
-        };
+        let f = (*v - median).abs();
         fuel += f;
     }
     fuel
 }
 
-fn part2(values: &mut Vec<u32>) -> i32 {
+fn part2(values: &mut Vec<i32>) -> i32 {
     let mut meanf: f64 = 0.0;
     for v in (&values).iter() {
         meanf += (*v as f64) / (values.len() as f64);
     }
-    let mean = meanf.floor() as u32;
+    let mean = meanf.floor() as i32;
     min(get_fuel(values, mean), get_fuel(values, mean + 1))
 }
 
@@ -47,10 +46,10 @@ fn min(a: i32, b: i32) -> i32 {
     return b;
 }
 
-fn get_fuel(values: &Vec<u32>, target: u32) -> i32 {
+fn get_fuel(values: &Vec<i32>, target: i32) -> i32 {
     let mut fuel: i32 = 0;
     for v in (&values).iter() {
-        let n = (target as i32 - *v as i32).abs();
+        let n = (target - *v).abs();
         fuel += get_fuel_n(n);
     }
     fuel
